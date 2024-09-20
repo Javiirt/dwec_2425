8eb5f74e2f57ebbbe145da0e84f166ac1022cc72
Javiirt <javivirodri3702@gmail.com>
2024-09-20 14:49:53 +0200

    Actividades webse

---
+++ tema-01/ejemplos/myScript.js
@@ -0,0 +1,10 @@
+//myScript.js
+function alerta() {
+    alert ("Hola mundo");
+	console.log("Hola mundo");
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+
+}
